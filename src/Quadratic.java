bafca2a2c6fc179d30e8c63032de0614bafa841e
Justin Lee <555960@my.cuhsd.org>
2022-12-13 20:17:35 -0800

    Add comments for the files

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,61 +1,105 @@
+/**
+ * @author Justin Lee, AP CSA P1
+ */
+
 public class Quadratic {
-    private int a;
-    private int b;
-    private int c;
+    // Instance variables
+    private int a; // Coefficient "a"; coefficient of the first term.
+    private int b; // Coefficient "b"; coefficient of the second term.
+    private int c; // Term "c"; the constant
     private int numRoots;
 
+    /**
+     * Constructs a Quadratic object that initializes the coefficients and the number of roots.
+     * @param a First term's coefficient
+     * @param b Second term's coefficient
+     * @param c The third term; the constant
+     */
     public Quadratic(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
 
+        // Finds the number of roots based on the discriminant of the quadratic.
         if (discriminant() > 0) {
             numRoots = 2;
         }
         else if (discriminant() == 0) {
             numRoots = 1;
         }
-        else {
+        else { // if the discriminant is < 0
             numRoots = 0;
         }
     }
 
+    /**
+     * Returns the value of the first coefficient.
+     * @return Coefficient "a"; the first term's coefficient
+     */
     public int getCoeffA() {
         return a;
     }
 
+    /**
+     * Returns the value of the second coefficient.
+     * @return Coefficient "b"; the second term's coefficient
+     */
     public int getCoeffB() {
         return b;
     }
 
+    /**
+     * Returns the value of the third term.
+     * @return Term "c"; the constant
+     */
     public int getCoeffC() {
         return c;
     }
 
+    /**
+     * Returns how many roots the quadratic has.
+     * @return The number of the quadratic's roots.
+     */
     public int getNumRoots() {
         return numRoots;
     }
 
+    /**
+     * Gets the value of the quadratic's discriminant. Used to find the number of roots and in the quadratic formula.
+     * @return The value of the discriminant.
+     */
     public int discriminant() {
         return b*b - 4*a*c;
     }
 
+    /**
+     * Makes the roots of the polynomial into String form; makes it readable by the user.
+     * @return String for the polynomial's roots.
+     */
     public String returnString() {
-        if (numRoots == 2) {
+        if (numRoots == 2) { // Returns both of the roots using the positive and negative versions of the quadratic formula.
             return "x = " + quadraticFormulaPos() + ", x = " + quadraticFormulaNeg();
         }
-        else if (numRoots == 1) {
+        else if (numRoots == 1) { // Returns the quadratic's single root by using the positive version of the quadratic formula (positive or negative ver. doesn't matter).
             return "x = " + quadraticFormulaPos();
         }
-        else {
+        else { // Returns NO REAL SOLUTIONS if there are no roots.
             return "No real solutions";
         }
     }
 
+    /**
+     * Returns the value of a root using the positive version of the Quadratic Formula.
+     * @return The double value of the root using the positive version.
+     */
     public double quadraticFormulaPos() {
         return (-1*b+Math.sqrt(discriminant()))/(2*a);
     }
 
+    /**
+     * Returns the value of a root using the negative version of the Quadratic Formula.
+     * @return The double value of the root using the negative version.
+     */
     public double quadraticFormulaNeg() {
         return (-1*b-Math.sqrt(discriminant()))/(2*a);
     }
--- src/QuadraticRootsCalc.java
+++ src/QuadraticRootsCalc.java
@@ -1,15 +1,24 @@
+/**
+ * @author Justin Lee, AP CSA P1
+ */
+
 import java.util.Scanner;
 
 public class QuadraticRootsCalc {
     public static void main(String[] args) {
 
+        // Gets input from the user in a while(true) loop; the quadratic expression can be entered any number of times without rerun.
         Scanner in = new Scanner(System.in);
         while (true) {
             System.out.println("What is your quadratic expression (ax^2 + bx + c)? Enter \"quit\" to exit and enter \"help\" for formatting/input help.");
             String expression = in.nextLine();
+
+            // If the entered input is "quit", the program closes.
             if (expression.equals("quit")) {
                 break;
             }
+
+            // If the entered input is "help", the program prints out a text for formatting help.
             if (expression.equals("help")) {
                 System.out.println("************************************************* FORMATTING HELP *************************************************");
                 System.out.println("Your input should be a quadratic trinomial expression in the form of \"ax^2 + bx + c\".");
@@ -22,83 +31,82 @@ public class QuadraticRootsCalc {
                 System.out.println("");
                 continue;
             }
+
+            // Prints out the quadratic's roots using produceAnswer.
             System.out.println(produceAnswer(expression));
             System.out.println();
         }
     }
 
+    /**
+     * A method that produces an answer (a String for the # of roots and the root(s)'s value(s)) using the Quadratic class.
+     * @param input The raw user input
+     * @return The String containing the number of roots and their values OR an error message.
+     */
     public static String produceAnswer(String input) {
+        // Splits the input up with a delimiter for " ", which splits up the terms and the operators.
         Scanner expression = new Scanner(input);
         expression.useDelimiter(" ");
 
-        int[] coefficients = {1, 1, 1};
+        // Since the code would be repeated when finding the coefficients for the 1st and 2nd term, a loop goes through the Scanner and saves the coefficients' values through an array.
+        int[] coefficients = {1, 1, 1}; // Arrays to save the values for the coefficients and operators.
         String[] operators = {"+", "+"};
         String currentOperator = "";
 
         // Getting the first and second coefficient
-        for (int k = 0; k < 2; k++) {
-            if (!expression.hasNext()){
+        for (int k = 0; k < 2; k++) { // Loops through 2 times to capture the 1st and 2nd terms and operators.
+            if (!expression.hasNext()){ // ERROR CHECK: If there isn't an input, returns an error message.
                 return "ERROR: Input is in an invalid format.";
             }
 
+            // Initializes temporary variables for the coefficient and term.
             String coeff = "";
             String term = expression.next();
 
-            // Uses a loop to get the coefficient as a string
+            // Uses a loop to get the coefficient as a string.
             int i;
-            for (i = 0; i < term.length(); i++) {
+            for (i = 0; i < term.length(); i++) { // Adds everything before the "x" to the coefficient String.
                 if (term.charAt(i) == 'x') {
                     break;
                 }
                 coeff += term.charAt(i);
             }
-            //System.out.println("Coefficient string for number " + (k+1) + ": " + coeff);
-            // Checks to see if there is no coefficient; initializes to 1
-            if (i == 0) {
+
+            // Tests for each case of coefficient to save a coefficient value in the array.
+            if (i == 0) { // Checks to see if there is no coefficient; initializes to 1
                 coefficients[k] = 1;
             }
-            // Checks to see if there is no numeric value in the coefficient but has a negative; initializes to -1
-            else if (coeff.equals("-")) {
+            else if (coeff.equals("-")) { // Checks to see if there is no numeric value in the coefficient but has a negative; initializes to -1
                 coefficients[k] = -1;
             }
-            // Getting the value of the coefficient
-            else {
+            else { // Getting the value of the coefficient
                 coefficients[k] = Integer.parseInt(coeff);
             }
 
-            //System.out.println("BEFORE CHANGING The operator for number " + (k+1) + ": " + operators[k]);
-            //System.out.println("expression.next: " + currentOperator);
-
             // Getting the operator between the terms
             currentOperator = expression.next();
-            if (!currentOperator.equals("+") && !currentOperator.equals("-")) {
+            if (!currentOperator.equals("+") && !currentOperator.equals("-")) { // ERROR CHECK: If the operator isn't addition or subtraction, returns an error message.
                 return "ERROR: Input is in an invalid format.";
             }
-            else {
+            else { // Saves the value of the operator.
                 operators[k] = currentOperator;
             }
-            //System.out.println("The operator for number " + (k+1) + ": " + operators[k]);
-
         }
 
         // Getting the third term
-        if (!expression.hasNext()){
+        if (!expression.hasNext()){ // ERROR CHECK: If there isn't an input for the third term, returns an error message.
             return "ERROR: Input is in an invalid format.";
         }
         coefficients[2] = Integer.parseInt(expression.next());
 
         // Factoring in the operators
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) { // Loops through the operators array and makes the coefficient negative if the operator is subtraction.
             if (operators[i].equals("-")) {
                 coefficients[i+1] *= -1;
             }
         }
 
-        // Calculating the roots
-        //System.out.println("First coeff: " + coefficients[0]);
-        //System.out.println("Second coeff: " + coefficients[1]);
-        //System.out.println("Third coeff: " + coefficients[2]);
-
+        // Calculating the roots using a Quadratic object.
         Quadratic quad = new Quadratic(coefficients[0], coefficients[1], coefficients[2]);
         return "Number of solutions: " + quad.getNumRoots() + "\n" + quad.returnString();
     }
